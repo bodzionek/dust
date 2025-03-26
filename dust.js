8afa125f7df91340e3c6c36324a22d3e12e26116
Bodzionek <pebe83@gmail.com>
2025-03-26 20:38:07 +0100

    Updated values

--- dust.js
+++ dust.js
@@ -13,13 +13,19 @@ const DEFAULT_CONFIG = [
     count: 80,
     radius: 5,
     speed: 8,
-    alpha: 0.07,
+    alpha: 0.15,
   },
   {
     count: 80,
     radius: 5,
     speed: 2,
-    alpha: 0.07,
+    alpha: 0.08,
+  },
+  {
+    count: 80,
+    radius: 5,
+    speed: 6,
+    alpha: 0.09,
   },
   {
     count: 60,
@@ -44,6 +50,12 @@ const DEFAULT_CONFIG = [
     radius: 60,
     speed: 10,
     alpha: 0.02,
+  },
+  {
+    count: 3,
+    radius: 90,
+    speed: 25,
+    alpha: 0.01,
   },
 ];
 
@@ -120,6 +132,8 @@ class DustParticle {
     this.spawnDelay = 0;
     this.cycle = 0;
     this.isAlive = false;
+    this.isSpawning = false;
+    this.isDying = false;
     this.position = null;
     this.vector = null;
 
@@ -129,7 +143,10 @@ class DustParticle {
   initParticleValues() {
     this.lifeSpan = getRandomInt(300, 2000);
     this.spawnDelay = getRandomInt(100, 500);
+    this.isSpawning =
+      this.radius < this.initRadius || this.alpha < this.initAlpha;
     this.isAlive = true;
+    this.isDying = false;
     this.cycle = 0;
     this.position = {
       x: getRandomInt(0, this.canvas.width),
@@ -154,13 +171,12 @@ class DustParticle {
         this.position.y,
         Math.max(this.radius - 1, 0)
       );
-      radgrad.addColorStop(0, `rgba(255,255,255,${this.alpha})`);
-      radgrad.addColorStop(
-        0.7,
-        `rgba(255,255,255,${Math.max(this.alpha - 0.01, 0.01)})`
-      );
+      radgrad.addColorStop(0, `rgba(255,255,255,0.01)`);
       radgrad.addColorStop(0.9, `rgba(255,255,255,${this.alpha})`);
-      radgrad.addColorStop(1, `rgba(255,255,255,0.01)`);
+      radgrad.addColorStop(
+        1,
+        `rgba(255,255,255,${Math.max(this.alpha - 0.02, 0.01)})`
+      );
       this.ctx.fillStyle = radgrad;
       this.ctx.arc(
         this.position.x,
@@ -178,9 +194,8 @@ class DustParticle {
   }
 
   update() {
-    if (this.isAlive && this.cycle === this.lifeSpan) {
-      this.isAlive = false;
-      this.cycle = 1;
+    if (this.isAlive && this.lifeSpan - this.cycle <= 120) {
+      this.isDying = true;
     }
     if (!this.isAlive && this.cycle >= this.spawnDelay) {
       this.initParticleValues();
@@ -193,19 +208,38 @@ class DustParticle {
 
   updateColorAndSize() {
     if (!this.isAlive) return;
-    if (this.lifeSpan - this.cycle <= 120) {
+
+    if (this.isSpawning) {
+      this.alpha = Math.min(this.alpha + this.initAlpha / 120, this.initAlpha);
+
+      if (this.alpha >= this.initAlpha) {
+        this.isSpawning = false;
+      }
+      return;
+    }
+
+    if (this.isDying) {
       this.alpha = Math.max(this.alpha - this.initAlpha / 120, 0);
-      this.radius = Math.max(
-        this.radius - this.initRadius / 120,
-        this.initRadius / 2
-      );
+
+      if (this.alpha === 0) {
+        this.isDying = false;
+        this.isAlive = false;
+        this.cycle = 1;
+      }
+
+      return;
+    }
+
+    const randomizer = getRandomInt(0, 10);
+    const modifier = randomizer === 10 ? -1 : randomizer === 9 ? 1 : 0;
+    const newRadius = this.radius + (this.initRadius / 60) * modifier;
+    const newAlpha = this.alpha + (this.initAlpha / 120) * modifier;
+    if (modifier === -1) {
+      this.radius = Math.max(newRadius, this.initRadius * 0.5);
+      this.alpha = Math.max(newAlpha, this.initAlpha - 0.02);
     } else {
-      if (this.alpha < this.initAlpha) {
-        this.alpha += this.initAlpha / 120;
-      }
-      if (this.radius < this.initRadius) {
-        this.radius += this.initRadius / 120;
-      }
+      this.radius = Math.min(newRadius, this.initRadius * 1.3);
+      this.alpha = Math.min(newAlpha, this.initAlpha + 0.02);
     }
   }
 
